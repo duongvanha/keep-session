8f76859595c31169f9eb6e679995540726934c98
duongha <haduong@beeketing.net>
2021-02-05 06:00:39 +0700

    update keep session

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 type Request struct {
@@ -21,23 +20,22 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	client := &http.Client{}
-
-	// Declare HTTP Method and Url
-	req, err := http.NewRequest("Get", "https://hive.shopbase.com/admin", nil)
+	req, err := http.NewRequest("GET", "https://hive.shopbase.com/admin", nil)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte(err.Error()))
 		return
 	}
-	// Set cookie
 	req.Header.Set("Cookie", fmt.Sprintf("PHPSESSID=%s", request.User))
+
 	res, err := client.Do(req)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte(err.Error()))
 		return
 	}
+	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
 		data, err := ioutil.ReadAll(res.Body)
@@ -61,5 +59,5 @@ func main() {
 
 	http.HandleFunc("/tick", hello)
 
-	_ = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	_ = http.ListenAndServe(":8080", nil)
 }
