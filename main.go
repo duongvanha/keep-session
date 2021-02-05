3884d26ee453a86511e8e80d598fbb8d2d252a3d
duongha <haduong@beeketing.net>
2021-02-05 20:52:07 +0700

    update ping ip

--- main.go
+++ main.go
@@ -7,8 +7,21 @@ import (
 	"net/http"
 )
 
+type Session struct {
+	Domain   string `json:"domain"`
+	HostOnly bool   `json:"hostOnly"`
+	HttpOnly bool   `json:"httpOnly"`
+	Name     string `json:"name"`
+	Path     string `json:"path"`
+	SameSite string `json:"sameSite"`
+	Secure   bool   `json:"secure"`
+	Session  bool   `json:"session"`
+	StoreId  string `json:"storeId"`
+	Value    string `json:"value"`
+}
+
 type Request struct {
-	User string `json:"user"`
+	User Session `json:"user"`
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -20,14 +33,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://hive.shopbase.com/admin", nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/admin", request.User.Domain), nil)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte(err.Error()))
 		return
 	}
-	req.Header.Set("Cookie", fmt.Sprintf("PHPSESSID=%s", request.User))
+	req.Header.Set("Cookie", fmt.Sprintf("PHPSESSID=%s", request.User.Value))
 
 	res, err := client.Do(req)
 	if err != nil {
